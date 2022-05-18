3001805bf697ec44db11763a29caa4da27cd9fdd
tlegen01 <93571334+tlegen01@users.noreply.github.com>
2022-05-18 21:41:44 +0600

    123

--- calc.py
+++ calc.py
@@ -1,13 +1,31 @@
-def calk(number1, number2, operation):
+import math
+def calk(number1, operation, number2 = 10,):
     print(number1, number2, operation)
     if operation == "+":
         return number1 + number2
     if operation == "-":
         return number1 - number2
+    if operation == "*":
+        return number1 * number2
+    if operation == "/":
+        if number1 != 0:
+            return number1 / number2
+        else:
+            print("vy vveli 0")
+            return 0
+    if operation == "**":  # vozvedenie v stepen
+        return number1 ** number2
+    if operation == "//":  # Delenie bez ostatka
+        return number1 // number2
+    if operation == "%":
+        return number1 % number2
+    if operation == "///":
+        return math.sqrt(number1)
 
 
-result = calk(10, 3, "-")
+
+result = calk(9, "/",)
 print(result)
 
-result = calk(-11, -5, "-")
-print(result)
+
+
