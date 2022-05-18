e5d2aa1a5590514eac68fc8d47ab89e8f24c58ea
tlegen01 <93571334+tlegen01@users.noreply.github.com>
2022-05-18 20:09:45 +0600

    update

---
+++ function.py
@@ -0,0 +1,18 @@
+# funkciya
+def say_hi():
+    print("Ya kupil Kota" + "1")
+
+say_hi()
+
+# print("Ya kupil Kota1")
+# print("Ya kupil Kota2")
+# print("Ya kupil Kota3")
+# print("Ya kupil Kota4")
+
+# var_int1 = 12
+# while var_int1:
+#     var_int1 = var_int1 + 1
+#     print(var_int1)
+#     if var_int1 == 500:
+#         print('my doshli do 500')
+# print("Cikl zavershen")
